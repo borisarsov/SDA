2b8f9fb80725f72fc44c161a3fbb8b8431e7dd43
borisarsov <33627435+borisarsov@users.noreply.github.com>
2017-11-17 12:31:08 +0200

    Add homework 1

---
+++ homework1/Sloznostnafibonachi.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+int fib(int x)
+{
+if(x==1 ||x==2)
+{
+    return 1;
+}
+else
+    return fib(x-1)+fib(x-2);
+}
+int Fibn(int n)
+{
+    int F[n];
+    F[0]=1;
+    F[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        F[i]=F[i-1]+F[i-2];
+    }
+
+    return F[n-1];
+
+
+}
+
+int main()
+{
+
+    return 0;
+}

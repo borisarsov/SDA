65d0923686a76b5d345833e2fe8fd2231a4a558f
borisarsov <33627435+borisarsov@users.noreply.github.com>
2018-01-22 14:23:34 +0100

    Add files via upload
    Utaksh in garden

--- homework4/main.cpp
+++ homework4/main.cpp
@@ -1,69 +1,48 @@
-#include <vector>
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
-vector<int> graph[100001];
-bool visited[100001];
-int pre[100001];
-int pos[100001];
-int dfsnumber = 1;
 
-const int MOVE_TOWARDS = 0;
-const int MOVE_AWAY = 1;
-
-void dfs(int node)
-{
-	visited[node] = true;
-	pre[node] = dfsnumber++;
-
-	for (int adj=0:adj<graph[node])
-	{
-		if (!visited[adj])
-		{
-			dfs(adj);
-		}
-	}
-
-	pos[node] = dfsnumber++;
-}
 
 int main()
 {
-	/*ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);*/
+	int citiesCnt;
+	cin >> citiesCnt;
 
-	int nodes;
-	cin >> nodes;
-
-	for (int i = 0; i < nodes - 1; ++i)
+	int roads[100][100];
+	for (int i = 0; i < citiesCnt; i++)
 	{
-		int parent, child;
-		cin >> parent >> child;
-		graph[parent].push_back(child);
-	}
-
-	dfs(1);
-
-	int queriesCount;
-	cin >> queriesCount;
-
-	for (size_t i = 0; i < queriesCount; i++)
-	{
-		int move, startNode, endNode;
-		cin >> move >> endNode >> startNode;
-
-		if (move == MOVE_TOWARDS)
+		for (int j = 0; j < citiesCnt; j++)
 		{
-			cout << (pre[startNode] > pre[endNode] && pos[startNode] < pos[endNode] ?
-				"Yes\n" : "No\n");
-		}
-		else if (move == MOVE_AWAY)
-		{
-			cout << (pre[startNode] < pre[endNode] && pos[startNode] > pos[endNode] ?
-				"Yes\n" : "No\n");
+			cin >> roads[i][j];
 		}
 	}
 
+	int count = 0, gardensCnt = 0;
+	for (int j = 0; j < citiesCnt; j++)
+	{
+		for (int p = j + 1; p < citiesCnt; p++)
+		{
+			for (int k = 0; k < citiesCnt; k++)
+			{
+				if (roads[j][k] == 1)
+				{
+					if (roads[j][k] == roads[p][k])
+					{
+						count++;
+					}
+				}
+			}
+			if (count >= 2)
+			{
+				gardensCnt++;
+				count = 0;
+			}
+		}
+		count = 0;
+	}
+
+	gardensCnt = gardensCnt / 2;
+	cout << gardensCnt << endl;
 	return 0;
 }
